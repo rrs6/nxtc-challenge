2001aebebccd3f72b29f94c0443752eed6d0b966
rrs6 <rrs6@cin.ufpe.br>
2025-05-16 22:33:58 -0300

    feat: initialize project with base files.

---
+++ nxtc-api/src/main/java/com/nxtc/nxtc_api/NxtcApiApplication.java
@@ -0,0 +1,13 @@
+package com.nxtc.nxtc_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NxtcApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NxtcApiApplication.class, args);
+	}
+
+}
