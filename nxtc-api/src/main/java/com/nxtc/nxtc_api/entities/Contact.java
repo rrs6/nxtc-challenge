a3325ddb72f5cee2a27fcec7bf227b6e898a26f9
rrs6 <rrs6@cin.ufpe.br>
2025-05-17 09:24:52 -0300

    feat: create contact entity.

---
+++ nxtc-api/src/main/java/com/nxtc/nxtc_api/entities/Contact.java
@@ -0,0 +1,50 @@
+package com.nxtc.nxtc_api.entities;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="contato")
+@Getter
+@Setter
+@AllArgsConstructor
+public class Contact {
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
+    @Column(name="contato_id")
+    private long id;
+
+    @Column(name="contato_nome")
+    private String nome;
+
+    @Column(name="contato_email")
+    private String email;
+
+    @Column(name="contato_celular")
+    private String celular;
+
+    @Column(name="contato_telefone")
+    private String telefone;
+
+    @Column(name="contato_sn_favorito")
+    private boolean favorito;
+
+    @Column(name="contato_sn_ativo")
+    private boolean ativo;
+
+    @Column(name="contato_dh_cad")
+    private Date createdAt;
+
+    public Contact() {
+        this.createdAt = new Date();
+    }
+}
