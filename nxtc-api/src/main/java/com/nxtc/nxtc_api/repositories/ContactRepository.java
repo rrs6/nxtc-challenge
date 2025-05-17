8cbf1bb5c1b74e118511605e78ce1e5d38821a7b
rrs6 <rrs6@cin.ufpe.br>
2025-05-17 09:25:19 -0300

    feat: create contact repository.

---
+++ nxtc-api/src/main/java/com/nxtc/nxtc_api/repositories/ContactRepository.java
@@ -0,0 +1,14 @@
+package com.nxtc.nxtc_api.repositories;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.nxtc.nxtc_api.entities.Contact;
+
+@Repository
+public interface ContactRepository extends JpaRepository<Contact, Long> {
+    public Optional<Contact> findByCelular(String celular);
+    public Optional<Contact> findById(long id);
+}
