ac0ce24e3b3285be4f598cd04241d7f7351bd373
rrs6 <rrs6@cin.ufpe.br>
2025-05-19 13:09:53 -0300

    feat: create an angular application.

--- nxtc-frontend/src/app/app.routes.ts
+++ nxtc-frontend/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { NewContactComponent } from './components/new-contact/new-contact.component';
+import { ContactHomeComponent } from './components/contact-home/contact-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'contact/new', component: NewContactComponent },
+    { path: 'contact', component: ContactHomeComponent},
+    { path: '**', redirectTo: 'contact'}
+];
