e4a65e4dd5151a832d378ec955207945aa4a1a78
rrs6 <rrs6@cin.ufpe.br>
2025-05-19 14:34:40 -0300

    fix: enable CORS in localhost:4200 frontend client address.

---
+++ nxtc-api/src/main/java/com/nxtc/nxtc_api/config/SpringWebConfig.java
@@ -0,0 +1,19 @@
+package com.nxtc.nxtc_api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class SpringWebConfig {
+    @Bean
+	public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+			}
+		};
+	}
+}
