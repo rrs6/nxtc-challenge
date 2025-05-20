761ee8873eaba47b6a2f8f593ecb078359ab4bed
rrs6 <rrs6@cin.ufpe.br>
2025-05-20 08:33:44 -0300

    feat: add contact home page to list all contacts and display their information.

--- nxtc-frontend/src/app/components/contact-home/contact-home.component.ts
+++ nxtc-frontend/src/app/components/contact-home/contact-home.component.ts
@@ -1,25 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 import { ContactService } from '../../services/ContactService';
 import { Contact } from '../../models/Contact';
+import { Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { ContactFieldsName } from '../../enums/ContactFieldsName';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-contact-home',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './contact-home.component.html',
   styleUrl: './contact-home.component.css'
 })
-export class ContactHomeComponent implements OnInit{
-  contacts: Contact[] = [];
-  loading: boolean = true;
-  constructor(private contactService: ContactService) {}
-  ngOnInit(): void {
-    this.contacts = this.getAllContact();
-    console.log(this.contacts);
-    this.loading = false;
-  }
 
-  getAllContact(): Contact[] {
-    return this.contactService.getAllContact();
+export class ContactHomeComponent {
+  public BooleanLabels: { [key: string]: string } = {
+    'true': 'Sim',
+    'false': 'Não'
+  };
+
+  public $contacts: Observable<Contact[]> = of([]);
+  public contactHeaders = Object.values(ContactFieldsName);
+
+  constructor(private contactService: ContactService) {
+    this.$contacts = this.contactService.getAllContact();
   }
 
 }
