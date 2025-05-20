5edd769dfdfffa6a7527596a5a3b2ffbf63a2c10
rrs6 <rrs6@cin.ufpe.br>
2025-05-20 08:31:15 -0300

    feat: add edit contact information route.

--- nxtc-frontend/src/app/app.routes.ts
+++ nxtc-frontend/src/app/app.routes.ts
@@ -1,9 +1,11 @@
 import { Routes } from '@angular/router';
 import { NewContactComponent } from './components/new-contact/new-contact.component';
 import { ContactHomeComponent } from './components/contact-home/contact-home.component';
+import { EditContactInfoComponent } from './components/edit-contact-info/edit-contact-info.component';
 
 export const routes: Routes = [
     { path: 'contact/new', component: NewContactComponent },
     { path: 'contact', component: ContactHomeComponent},
+    { path: 'contact/:id', component: EditContactInfoComponent},
     { path: '**', redirectTo: 'contact'}
 ];
