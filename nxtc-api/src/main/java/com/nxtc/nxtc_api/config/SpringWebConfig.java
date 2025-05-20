e29f024da224a2303f80f94750f41df4d8502d91
rrs6 <rrs6@cin.ufpe.br>
2025-05-20 08:26:32 -0300

    fix: allow all HTTP methods used in the API to avoid CORS issues.

--- nxtc-api/src/main/java/com/nxtc/nxtc_api/config/SpringWebConfig.java
+++ nxtc-api/src/main/java/com/nxtc/nxtc_api/config/SpringWebConfig.java
@@ -12,7 +12,9 @@ public class SpringWebConfig {
         return new WebMvcConfigurer() {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT")
+                .allowedHeaders("*");
 			}
 		};
 	}
