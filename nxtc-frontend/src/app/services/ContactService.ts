47f2568299f4c8dfe20db912a1a61433fd75e9d0
rrs6 <rrs6@cin.ufpe.br>
2025-05-20 08:39:47 -0300

    feat: create contact service to handle all contact-related business logic.

--- nxtc-frontend/src/app/services/ContactService.ts
+++ nxtc-frontend/src/app/services/ContactService.ts
@@ -1,16 +1,39 @@
 import { Injectable } from "@angular/core";
 import { HttpService } from "./HttpService";
 import { Contact } from "../models/Contact";
+import { map, Observable } from "rxjs";
+import { ContactDTO } from "../dtos/ContactDTO";
 
-@Injectable({providedIn: "root"})
+@Injectable({ providedIn: "root" })
 export class ContactService {
-    constructor(private httpService: HttpService) {}
+    constructor(private httpService: HttpService) { }
 
-    public getAllContact(): Contact[] {
-        let allContacts: Contact[] = [];
-        this.httpService.get('contact').subscribe((contacts) => allContacts = contacts as Contact[]);
-        return allContacts;
+    public getAllContact(): Observable<Contact[]> {
+        return this.httpService.get<any[]>('contact').pipe(
+            map(response =>
+                response.map(
+                    contact => new Contact(
+                        contact.id,
+                        contact.name,
+                        contact.email,
+                        contact.telephone,
+                        contact.cellphone,
+                        contact.favorite,
+                        contact.active
+                    )
+                )
+            )
+        );
+    }
+    public createNewContact(contact: ContactDTO): Observable<number> {
+        return this.httpService.post<number>('contact', contact);
     }
 
+    public getContactById(id: string): Observable<ContactDTO> {
+        return this.httpService.get(`contact/${id}`);
+    }
 
+    public editContactInfo(id: string, contact: ContactDTO): Observable<ContactDTO> {
+        return this.httpService.put(`contact/${id}`, contact);
+    }
 }
