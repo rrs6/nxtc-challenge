0bc8947909f313854a2818d643f546acc5d56179
rrs6 <rrs6@cin.ufpe.br>
2025-05-20 08:38:59 -0300

    feat: create enum for contact table headers on home page.

---
+++ nxtc-frontend/src/app/enums/ContactFieldsName.ts
@@ -0,0 +1,9 @@
+export enum ContactFieldsName {
+    id = "Id",
+    name = "Nome",
+    email = "Email",
+    telephone = "Telefone",
+    cellphone = "Celular",
+    active = "Ativo",
+    favorite = "Favorito"
+};
